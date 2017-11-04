1148988f42fcc6cff9a37bd6243126bff09eb81c
Bettina <b.griessbach@gmail.com>
2017-11-04 18:04:10 +0000

    tables for the win

---
+++ animation.js
@@ -0,0 +1,4 @@
+var e = document.getElementById('table');
+e.onmouseover = function() {
+  document.getElementById('table_languages').innerHTML = "<img class="flags" src="images/Germany.png" alt="Icon for Germany"><img class="flags" src="images/Japan.png" alt="Icon for Japan"><img class="flags" src="images/United-Kingdom.png" alt="Icon for UK">";
+}
