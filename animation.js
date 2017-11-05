eb6805ef22ce0e6c501b24386e4528a74b73b626
bgriessbach <b.griessbach@gmail.com>
2017-11-05 11:56:54 +0000

    Update animation.js

--- animation.js
+++ animation.js
@@ -6,22 +6,22 @@ languages.addEventListener("mouseover", function(event) {
 
 var extension = document.getElementById('table_extension');
 extension.addEventListener("mouseover", function(event) {
-  document.getElementById('display_extension').innerHTML = '<img class="screenshot" src="images/layout_New.PNG" alt="Screenshot of Chrome Extension"><br /> a Chrome Weather Forecast Extension';
+  document.getElementById('display_extension').innerHTML = '<img class="picture" src="images/layout_New.PNG" alt="Screenshot of Chrome Extension"><br /> a Chrome Weather Forecast Extension';
 });
 
 var jobs = document.getElementById('table_jobs');
 jobs.addEventListener("mouseover", function(event) {
-  document.getElementById('display_jobs').innerHTML = '<img src="images/it.gif" alt="IT Crowd Gif"> <br /> Escalated Global Technical Support <br /><img src="images/PMlogo.jpg" alt="Image for Project Management"> <br /> Project Management for Circuit Installations<br /><img src="images/BTTC.PNG" alt="Bloomberg Technical Training Certification"><br /> Planned and executed Technical Training Seminars';
+  document.getElementById('display_jobs').innerHTML = '<img class="picture" src="images/it.gif" alt="IT Crowd Gif"> <br /> Escalated Global Technical Support <br /><img class="picture" src="images/PMlogo.jpg" alt="Image for Project Management"> <br /> Project Management for Circuit Installations<br /><img class="picture" src="images/BTTC.PNG" alt="Bloomberg Technical Training Certification"><br /> Event Planner and Presenter for Technical Training Seminars';
 });
 
 var python = document.getElementById('table_python');
 python.addEventListener("mouseover", function(event) {
-  document.getElementById('display_python').innerHTML = '<img src="images/python_icon.png" alt="Python icon">';
+  document.getElementById('display_python').innerHTML = '<img class="picture" src="images/python_icon.png" alt="Python icon">';
 });
 
   var videos = document.getElementById('table_videos');
 videos.addEventListener("mouseover", function(event) {
-  document.getElementById('display_videos').innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/7nXfybcgm6g" frameborder="0" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/eQo_uOH5jSA" frameborder="0" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/zEj5Quzhktc" frameborder="0" allowfullscreen></iframe>';
+  document.getElementById('display_videos').innerHTML = '<iframe class="picture" width="560" height="315" src="https://www.youtube.com/embed/7nXfybcgm6g" frameborder="0" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/eQo_uOH5jSA" frameborder="0" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/zEj5Quzhktc" frameborder="0" allowfullscreen></iframe>';
 });
 
 }
